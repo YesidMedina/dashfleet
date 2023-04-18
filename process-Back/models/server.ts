43b3ef902520b0c9fee8809e875f5be309cb1066
YesidMedina <clarin.osb.andres@gmail.com>
2023-04-18 18:13:24 -0500

    servidor

--- process-Back/models/server.ts
+++ process-Back/models/server.ts
@@ -48,6 +48,14 @@ class Server {
     }
 
     router() {
+        this.app.use((req, res, next) => {
+            res.header('Access-Control-Allow-Origin', 'https://dashfleet.vercel.app'); // (*)update to match the domain you will make the request from
+            res.header('Access-Control-Allow-Credentials', 'true');
+            res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+            res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+            next();
+          });
+    
         this.app.use( this.apiPaths.clients, clientsRoutes),
         this.app.use( this.apiPaths.orders, ordersRoutes),
         this.app.use( this.apiPaths.products, productsRoutes)
